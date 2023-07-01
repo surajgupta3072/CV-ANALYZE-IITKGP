ecfffe2f45e97596ac474514ef9d9acdcf12fc81
Suraj Gupta <surajgupta.3072@gmail.com>
2023-07-02 04:02:29 +0530

    added chatresponse function

---
+++ aiapi.py
@@ -0,0 +1,21 @@
+import openai
+import config
+
+api_key = config.DevelopmentConfig.OPENAI_KEY
+openai.api_key = api_key
+
+def generateChatResponse(prompt):
+    messages =[]
+    messages.append(  {"role": "system", "content": "You are a helpful assistant."})
+
+    question ={}
+    question['role'] = 'user'
+    question['content'] = prompt
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
+    try:
+        answer = response['choices'][0]['message']['content']
+    except:
+        answer = "oops!"
+    return answer
+
+
