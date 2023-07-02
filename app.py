21d3f1a3ccd97921723e877bb709323c02d0a140
Suraj Gupta <surajgupta.3072@gmail.com>
2023-07-03 00:53:43 +0530

    Final changes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify, request
 import config
 import openai
 import aiapi
+import pdfplumber
+import requests
 
 
 def page_not_found(e):
@@ -14,17 +16,30 @@ app.config.from_object(config.config['development'])
 app.register_error_handler(404, page_not_found)
 
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/api/refine-cv', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-      prompt = request.form['prompt']
-      answer = aiapi.generateChatResponse(prompt)
-      res = {}
-      res['answer'] = answer
-      return jsonify(res), 200
+      file = request.files['file']
+      if file.filename.endswith('.pdf'):
+          # Extract text content from the PDF
+          with pdfplumber.open(file) as pdf:
+              text_content = ""
+              for page in pdf.pages:
+                  text_content += page.extract_text()
+
+          # Pass the extracted text to the GPT model
+          answer = aiapi.generateChatResponse(text_content)
+          res = {}
+          res['answer'] = answer
+          return jsonify(res), 200
+      else:
+          return "Invalid file format. Please upload a PDF file."
+      
 
     return render_template('index.html', **locals())
-
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
+
+    
